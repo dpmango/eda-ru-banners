ac55ee9bddb607617a8f451c09d5005a18dcdc3c
Sergey Khmelevskoy <xs290@me.com>
2019-08-21 23:51:22 +0300

    refactor components, removed extra js, add image

--- src/js/app.js
+++ src/js/app.js
@@ -22,46 +22,22 @@ var easingSwing = [0.02, 0.01, 0.47, 1]; // default jQuery easing
     app.init = function() {
       app.initGlobalPlugins();
       app.initPlugins();
-      app.initComponents();
-    };
-
-    app.onLoadTrigger = function() {};
-
-    app.refresh = function() {
-      app.initPlugins(true);
-      app.initComponents(true);
     };
 
     app.destroy = function() {};
 
-    // pjax triggers
-    app.newPageReady = function() {
-      app.refresh();
-    };
-
-    app.transitionCompleted = function() {
-      APP.Plugins.AOS.refresh();
-      app.onLoadTrigger();
-    };
-
     // Global plugins which must be initialized once
     app.initGlobalPlugins = function() {
       APP.Dev.Credentials.logCredentials();
-      APP.Dev.Breakpoint.listenResize();
       APP.Browser().methods.setBodyTags();
       APP.Plugins.LegacySupport.init();
-      APP.Plugins.ScrollBlock.listenScroll();
-      APP.Plugins.Clicks.init();
       APP.Plugins.AOS.init();
     };
 
     // Plugins which depends on DOM and page content
-    app.initPlugins = function(fromPjax) {
+    app.initPlugins = function() {
       APP.Plugins.LegacySupport.fixImages();
     };
-
-    // All components from `src/componenets`
-    app.initComponents = function(fromPjax) {};
 
     return app;
   };
@@ -69,11 +45,5 @@ var easingSwing = [0.02, 0.01, 0.47, 1]; // default jQuery easing
   // a.k.a. ready
   $(function() {
     APP.Initilizer().init();
-  });
-
-  $(window).on('load', function() {
-    $.ready.then(function() {
-      APP.Initilizer().onLoadTrigger();
-    });
   });
 })(jQuery, window.APP);
