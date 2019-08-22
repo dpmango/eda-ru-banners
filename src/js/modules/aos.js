a600cdb6e2db2c6a1d2a1a22864b039380630820
Sergey Khmelevskoy <xs290@me.com>
2019-08-22 17:05:20 +0300

    fix aos triiger point

--- src/js/modules/aos.js
+++ src/js/modules/aos.js
@@ -6,7 +6,7 @@
     init: function() {
       AOS.init({
         // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-        offset: 120, // offset (in px) from the original trigger point
+        offset: 0, // offset (in px) from the original trigger point
         delay: 0, // values from 0 to 3000, with step 50ms
         duration: 400, // values from 0 to 3000, with step 50ms
         easing: 'ease-in', // default easing for AOS animations
